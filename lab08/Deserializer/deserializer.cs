3f20f11c7a07ba248c9eeb9c52748e80ebf3af8e
DdaniilDev <daniil.s20035@mail.ru>
2022-11-19 02:28:22 +0300

    new

---
+++ lab08/Deserializer/deserializer.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+using Animals;
+
+XmlSerializer xmlSerializer = new XmlSerializer(typeof(Lion));
+
+using (FileStream fs = new FileStream("lion.xml", FileMode.OpenOrCreate))
+{
+    Lion? for_deserialization = xmlSerializer.Deserialize(fs) as Lion;
+    Console.WriteLine("Object has been deserialized successfully");
+    Console.WriteLine($"{for_deserialization.Country} {for_deserialization.Name} {for_deserialization.HideFromOtherAnimal} {for_deserialization.WhatAnimal}");
+}
---
+++ lab08/Serializer/serializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+using Animals;
+
+Animal for_serialization = new Lion("Russia", "Bob", false, eClassificationAnimal.Omnivores);
+
+XmlSerializer xmlSerializer = new XmlSerializer(typeof(Lion));
+
+using (FileStream fs = new FileStream("lion.xml", FileMode.OpenOrCreate))
+{
+    xmlSerializer.Serialize(fs, for_serialization);
+    Console.WriteLine("Object has been serialized succesfully");
+}
